0d49d719cf79c6234e3fa3c9f32678cdb0316053
victoriaturkel <victoria.turkel@gmail.com>
2025-04-17 09:08:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ async def setup_webhook():
 if __name__ == '__main__':
     import asyncio
     asyncio.run(setup_webhook())
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
